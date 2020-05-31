1716684409de48984ce510252b42e7159945e89f
Rachit Malik <rachitmalik1947@gmail.com>
2020-06-01 00:59:59 +0530

    added postgres image to docker-compose.yml, setup to connect to postgres db, Dockerfile for server (not used)

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,20 @@ const expressPino = require('express-pino-logger');
 const path = require('path');
 const logger = require('./utils/logger');
 const { ENVIRONMENT } = require('./utils/constants');
+const DB  = require('pg').Client
+const dbInstance = new DB({
+  user: "admin",
+  password: "password",
+  port: 5432, 
+  database: "clinicrecdb"
+})
+dbInstance.connect()
+.then(() => console.log("Connected to DB"))
+.catch(e => console.log(e))
+.finally(() => dbInstance.end())
+
+
+
 require('dotenv').config();
 
 const expressLogger = expressPino({ logger });
@@ -26,5 +40,6 @@ if (ENVIRONMENT === 'production' || ENVIRONMENT === 'dev') {
       res.sendFile(path.join(`${__dirname}/../../`, 'client', 'build', 'index.html'));
   });
 }
+
 
 module.exports = app;
