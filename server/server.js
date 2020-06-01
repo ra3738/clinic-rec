6b6e120442b753cbf1c5e5e473d7f7031d249752
Rachit Malik <rachitmalik1947@gmail.com>
2020-06-01 13:09:18 +0530

    updated gitignore

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const logger = require('./src/utils/logger');
 
-const port = 3001; 
+const port = process.env.PORT || 5001; 
 // For now 3001, because docker container is supposed to run at 3001 otherwise can use process.env.PORT 
 
 app.listen(port, () => {
