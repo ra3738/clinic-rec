13823610aa4c37287741ee9e6ad595d4e4377d31
Rachit Kumar Malik <rachitmalik1947@gmail.com>
2020-06-06 00:05:43 +0530

    Postgres setup (#3)
    * added postgres image to docker-compose.yml, setup to connect to postgres db, Dockerfile for server (not used)
    
    * added postgres image to docker-compose.yml, setup to connect to postgres db, Dockerfile for server (not used)
    
    * minor changes
    
    * add postgres data
    
    * fixed corrupted postgres
    
    * fixed corrupted postgres
    
    * updated gitignore
    
    * removed pg data
    
    * removed server dockerfile

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const app = require('./src/app');
 const logger = require('./src/utils/logger');
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5001; 
+// For now 3001, because docker container is supposed to run at 3001 otherwise can use process.env.PORT 
 
 app.listen(port, () => {
   logger.info(`Server is running on port: ${port}`);
