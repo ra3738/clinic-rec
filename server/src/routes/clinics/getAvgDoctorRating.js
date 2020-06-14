bae6db742b6a7df7382892310df1f991004d7ffc
isthisevenreal <rithvik.al@gmail.com>
2020-06-13 19:16:17 -0700

    Added doctor name for each rating

--- server/src/routes/clinics/getAvgDoctorRating.js
+++ server/src/routes/clinics/getAvgDoctorRating.js
@@ -3,7 +3,7 @@ const sqlClient = require('../../utils/sqlClient');
 
 router.get("/getAvgDoctorRating/:specialty", (req, res) => {
     //const doctorSpecialty = req.params.specialty; 
-    const query = 'select did, avg(stars) from public.rating r group by did;'
+    const query = 'select d.full_name, avg(r.stars) from public.rating r, public.doctor d where r.did = d.id group by d.full_name;'
     sqlClient.query(query, 
         (err, qres) => {
              if(err){
