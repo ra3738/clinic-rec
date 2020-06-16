9ffe24fdbeadb6a0cba8a27b5a6ed81334f8bf9e
Rachit Kumar Malik <rachitmalik1947@gmail.com>
2020-06-16 12:23:57 +0530

    Added dynamic col projection query (#24)

--- server/src/routes/clinicRouter.js
+++ server/src/routes/clinicRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const {CLINIC_ROUTE} = require('../utils/constants');
 const getClinicDetailsInCity = require('./clinics/getClinicDetailsInCity');
+const getDynamicClincDetails = require('./clinics/getDynamicClinicDetails');
 
 router.use(CLINIC_ROUTE, getClinicDetailsInCity);
+router.use(CLINIC_ROUTE, getDynamicClincDetails);
 
 module.exports = router;
