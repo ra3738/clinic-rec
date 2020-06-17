66271ce8b3b15a64102a83f21fd000f7191803d5
Rachit Malik <rachitmalik1947@gmail.com>
2020-06-18 01:52:18 +0530

    minor fix to adjust for schema change

--- server/src/routes/user/createUser.js
+++ server/src/routes/user/createUser.js
@@ -9,7 +9,7 @@ router.post('/patient/:id', async (req, res) => {
     const medHisResponse = await sqlClient.query(`INSERT INTO Medical_History (guardian_name, height, weight, date_created) VALUES ('', 0, 0, to_timestamp(${Date.now()} / 1000.0)) RETURNING *`);
     if (medHisResponse) {
       const medHisId = medHisResponse.rows[0].id;
-      const patientResponse = await sqlClient.query(`INSERT INTO Patient (id, mid, email, password, full_name, profile_picture_url) VALUES ('${req.params.id}', ${medHisId}, '${req.body.username}', '', '', '') RETURNING *`);
+      const patientResponse = await sqlClient.query(`INSERT INTO Patient (id, mid, email, full_name, profile_picture_url) VALUES ('${req.params.id}', ${medHisId}, '${req.body.username}', '', '') RETURNING *`);
       formatPatientResponse(res, patientResponse.rows[0], medHisResponse.rows[0]);
       return;
     }
